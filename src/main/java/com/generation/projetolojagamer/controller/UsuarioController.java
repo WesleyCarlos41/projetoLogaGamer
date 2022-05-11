7d83b3f391b8a0601c40e5bbf40c59d96cfd43cc
WesleyCarlos41 <102267247+WesleyCarlos41@users.noreply.github.com>
2022-05-11 16:06:33 -0300

    Adicionando put e delete

--- src/main/java/com/generation/projetolojagamer/controller/UsuarioController.java
+++ src/main/java/com/generation/projetolojagamer/controller/UsuarioController.java
@@ -9,12 +9,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.generation.projetolojagamer.model.Usuario;
 import com.generation.projetolojagamer.model.Usuario;
 import com.generation.projetolojagamer.model.UsuarioLogin;
 import com.generation.projetolojagamer.repository.UsuarioRepository;
@@ -52,6 +56,13 @@ public class UsuarioController {
 		return usuarioService.cadastraUsuario(usuario)
 				.map(resposta -> ResponseEntity.status(HttpStatus.CREATED).body(resposta))
 				.orElse(ResponseEntity.status(HttpStatus.BAD_REQUEST).build());
-
+	}
+	@PutMapping
+	public ResponseEntity<Usuario> put (@RequestBody Usuario usuario){
+		return ResponseEntity.status(HttpStatus.OK).body(usuarioRepository.save(usuario));
+	}
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable long id) {
+		usuarioRepository.deleteById(id);
 	}
 }
